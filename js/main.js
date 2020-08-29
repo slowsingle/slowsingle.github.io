43a2155280fcc489755a5582a975730d7fd473e4
UMA <ume0kanaz@gmail.com>
2020-08-29 16:30:26 +0900

    なんかいい感じになってきた

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'YesYes!'
+    message: 'Hello, World!',
+    postUrlList: [
+      'posts/post-2020xxxx.html',
+      'posts/post-2020yyyy.html',
+      'posts/post-2020zzzz.html',
+    ]
   }
 })
