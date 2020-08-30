03349809d6e9604baa515ffd0b9bf15036aa87f8
UMA <ume0kanaz@gmail.com>
2020-08-31 01:42:48 +0900

    posts配下のhtmlが開けないバグを解決

--- js/main.js
+++ js/main.js
@@ -2,14 +2,14 @@ Vue.component('header-component', {
   template: '\
     <header class="site-header"> \
       <nav class="navbar navbar-expand-lg navbar-dark bg-dark"> \
-        <a class="navbar-brand" href="index.html">MyPage</a> \
+        <a class="navbar-brand" href="/index.html">MyPage</a> \
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation"> \
           <span class="navbar-toggler-icon"></span> \
         </button> \
         <div class="collapse navbar-collapse" id="navbarNavDropdown"> \
           <ul class="navbar-nav"> \
             <li class="nav-item active"> \
-              <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a> \
+              <a class="nav-link" href="/index.html">Home <span class="sr-only">(current)</span></a> \
             </li> \
           </ul> \
         </div> \
@@ -39,7 +39,7 @@ var app = new Vue({
       let originUrl = location.origin;
       let postUrlList = [];
       for(let i=1; i<this.numOfPosts+1; i++) {
-        postUrlList.push(originUrl + "/" + String(i) + ".html");
+        postUrlList.push(originUrl + "/posts/" + String(i) + ".html");
       }
       return postUrlList;
     }
