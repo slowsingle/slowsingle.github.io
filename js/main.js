9284a9daf198406ba4a8fe534621b207742ddc00
umeda-m <ume0kanaz@gmail.com>
2020-09-05 15:41:25 +0900

    記事のタイトルを自動で読み込めるようにした

--- js/main.js
+++ js/main.js
@@ -2,14 +2,14 @@ Vue.component('header-component', {
   template: '\
     <header class="site-header"> \
       <nav class="navbar navbar-expand-lg navbar-dark bg-dark"> \
-        <a class="navbar-brand" href="/index.html">MyPage</a> \
+        <a class="navbar-brand" href="/">MyPage</a> \
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation"> \
           <span class="navbar-toggler-icon"></span> \
         </button> \
         <div class="collapse navbar-collapse" id="navbarNavDropdown"> \
           <ul class="navbar-nav"> \
             <li class="nav-item active"> \
-              <a class="nav-link" href="/index.html">Home <span class="sr-only">(current)</span></a> \
+              <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a> \
             </li> \
           </ul> \
         </div> \
@@ -33,15 +33,37 @@ var app = new Vue({
   data: {
     message: 'Hello, World!',
     numOfPosts: 3,
+    postInfo: [],
   },
-  computed: {
-    getPostUrlList: function() {
-      let originUrl = location.origin;
-      let postUrlList = [];
-      for(let i=1; i<this.numOfPosts+1; i++) {
-        postUrlList.push(originUrl + "/posts/" + String(i) + ".html");
-      }
-      return postUrlList;
+  created: function() {
+    const _home = ["/", "/index.html"]
+    if (!_home.includes(location.pathname)) {
+      console.log("Do Nothing.");
+      return;
     }
+
+    let originUrl = location.origin;
+    for(let i=1; i<this.numOfPosts+1; i++) {
+      let post = {};
+      const localUrl = "/posts/" + String(i) + ".html";
+      axios.get(localUrl, {
+        responseType: 'document',
+      })
+      .then((response) => {
+        // handle success
+        post.theme = response.data.getElementById("theme").innerHTML;
+        post.url = originUrl + localUrl;
+      })
+      .catch((error) => {
+        // handle error
+        post.theme = "No Name"
+        post.url = originUrl + localUrl;  // 404.htmlに飛ばしたい
+        console.log(error);
+      })
+      .then(() => {
+        this.postInfo.push(post);
+      })
+    }
+    console.log(this.postInfo);
   }
 })
